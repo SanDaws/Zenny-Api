2a28eac4fbd593f685da6029ddce9deca072e6f6
Daws <castrogiraldosantiago@gmail.com>
2024-09-05 16:56:57 -0500

    Feat: scaffolding whitout  db lickign

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zenny_Api.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int? SubscriptionTypesId { get; set; }
+
+    public virtual SubscriptionType? SubscriptionTypes { get; set; }
+}
