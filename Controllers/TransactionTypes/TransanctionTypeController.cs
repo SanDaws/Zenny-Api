59075a3d780a38a84b91f4418a545ea5cd790c73
jtorova5 <jtorova5@gmail.com>
2024-09-10 10:33:51 -0500

    feat: adding functionality to the GetTransactiontype  method

--- Controllers/TransactionTypes/TransanctionTypeController.cs
+++ Controllers/TransactionTypes/TransanctionTypeController.cs
@@ -1,8 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Zenny_Api.Data;
+using Zenny_Api.Models;
 
 namespace Zenny_Api.Controllers.TransactionTypes;
 
@@ -10,5 +9,19 @@ namespace Zenny_Api.Controllers.TransactionTypes;
 [Route("api/[controller]")]
 public class TransanctionTypeController : ControllerBase
 {
+    private readonly ILogger<TransanctionTypeController> _logger;
+    private readonly MovementDbContext _context;
 
+    public TransanctionTypeController(ILogger<TransanctionTypeController> logger, MovementDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    // GET api/transactionType
+    [HttpGet(Name = "GetTransactionTypes")]
+    public async Task<ActionResult<IEnumerable<TransactionType>>> GetTransactionTypes()
+    {
+        return await _context.TransactionTypes.ToListAsync();
+    }
 }
