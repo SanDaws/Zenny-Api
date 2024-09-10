f6c5c252035e5211a8cbdd4fb2107791e426c51f
jtorova5 <jtorova5@gmail.com>
2024-09-10 10:28:50 -0500

    feat: creation of 'TransactionTypes' folder and controller

---
+++ Controllers/TransactionTypes/TransanctionTypeController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zenny_Api.Controllers.TransactionTypes;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TransanctionTypeController : ControllerBase
+{
+
+}
