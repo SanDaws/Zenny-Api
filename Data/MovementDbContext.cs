d2b2bfa1ec19b3d877d8df7aa530dbc95598b044
luisaferRP <luisaramiresporras103@gmail.com>
2024-09-09 21:51:03 -0500

    Feact: se realizar scaffoldir de movement

---
+++ Data/MovementDbContext.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+using Zenny_Api.Models;
+
+namespace Zenny_Api.Data;
+
+public partial class MovementDbContext : DbContext
+{
+    public MovementDbContext()
+    {
+    }
+
+    public MovementDbContext(DbContextOptions<MovementDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Movement> Movements { get; set; }
+
+    public virtual DbSet<TransactionType> TransactionTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8_general_ci")
+            .HasCharSet("utf8");
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("categories");
+
+            entity.HasIndex(e => e.Category1, "category_UNIQUE").IsUnique();
+
+            entity.HasIndex(e => e.Id, "id_UNIQUE").IsUnique();
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+            entity.Property(e => e.Category1).HasColumnName("category");
+        });
+
+        modelBuilder.Entity<Movement>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("movements");
+
+            entity.HasIndex(e => e.CategoriesId, "categories_id");
+
+            entity.HasIndex(e => e.Id, "id_UNIQUE").IsUnique();
+
+            entity.HasIndex(e => e.TransactionTypesId, "transaction_types_id");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11) unsigned")
+                .HasColumnName("id");
+            entity.Property(e => e.CategoriesId)
+                .HasDefaultValueSql("'9'")
+                .HasColumnType("int(11)")
+                .HasColumnName("categories_id");
+            entity.Property(e => e.MovementDate).HasColumnName("movement_date");
+            entity.Property(e => e.TransactionTypesId)
+                .HasColumnType("int(11)")
+                .HasColumnName("transaction_types_id");
+            entity.Property(e => e.UserId)
+                .HasColumnType("int(11)")
+                .HasColumnName("user_id");
+            entity.Property(e => e.Value).HasColumnName("value");
+
+            entity.HasOne(d => d.Categories).WithMany(p => p.Movements)
+                .HasForeignKey(d => d.CategoriesId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("movements_ibfk_1");
+
+            entity.HasOne(d => d.TransactionTypes).WithMany(p => p.Movements)
+                .HasForeignKey(d => d.TransactionTypesId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("movements_ibfk_2");
+        });
+
+        modelBuilder.Entity<TransactionType>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("transaction_types");
+
+            entity.HasIndex(e => e.Id, "id_UNIQUE").IsUnique();
+
+            entity.HasIndex(e => e.TransactionType1, "transaction_type_UNIQUE").IsUnique();
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+            entity.Property(e => e.TransactionType1)
+                .HasMaxLength(100)
+                .HasColumnName("transaction_type");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zenny_Api.Models;
+
+public partial class Movement
+{
+    public uint Id { get; set; }
+
+    public DateOnly MovementDate { get; set; }
+
+    public int UserId { get; set; }
+
+    public double Value { get; set; }
+
+    public int CategoriesId { get; set; }
+
+    public int TransactionTypesId { get; set; }
+
+    public virtual Category Categories { get; set; } = null!;
+
+    public virtual TransactionType TransactionTypes { get; set; } = null!;
+}
