1c279b90915cd55a50004bf93197222a19163919
luisaferRP <luisaramiresporras103@gmail.com>
2024-09-10 22:07:33 -0500

    Feat: se crea metodo get search para buscar por una letra que se encuentre en nombre y apellido

--- Controllers/UsersControllers.cs
+++ Controllers/UsersControllers.cs
@@ -5,6 +5,7 @@ using Zenny_Api.Models;
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Zenny_Api.Controllers
@@ -33,8 +34,8 @@ namespace Zenny_Api.Controllers
         }
 
 
-        //metodo get por id
-        [HttpGet("{id}",Name = "GetUsuario")]
+        //metodo get por id (hacer uno donde busque tambien por una letra(buscador))
+        [HttpGet("{id}",Name = "GetUsuarioById")]
         public async Task<ActionResult<User>> GetUser(int id)
         {
             var user = await _context.Users.FindAsync(id);
@@ -47,15 +48,42 @@ namespace Zenny_Api.Controllers
             return user;
         }
 
+        //buscar por letra en nombre y apellido
+        [HttpGet("search/{letter}",Name = "GetUsuarioSearch")]
+        public async Task<ActionResult<User>> GetUserL(string letter)
+        {
+             // Verificar que se haya pasado una letra
+            if (string.IsNullOrEmpty(letter) || letter.Length != 1)
+            {
+                return BadRequest("Debe proporcionar una letra para la búsqueda.");
+            }
 
-        //metodo create controller  falta por revisar --------------------
+            var queryLetter = letter.ToLower();
+
+            var users = await _context.Users.ToListAsync();
+
+            //StartsWith: comprueba si una cadena de texto(string) comienza con una secuencia de caracteres específica,Devuelve un valor bool.
+            var result = users.Where(u => u.Name.StartsWith(queryLetter, StringComparison.OrdinalIgnoreCase) ||
+                            u.LastName.StartsWith(queryLetter, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (result.Count == 0)
+            {
+                return NotFound("No se encontraron coincidencias.");
+            }
+
+            return Ok(result);
+
+        }
+
+
+        //metodo create controller -(recordar validacion para que no muestre el campo id)
         [HttpPost]
         public async Task<ActionResult<User>> Post(User user)
         {
             _context.Add(user);
             await _context.SaveChangesAsync();
 
-            return new CreatedAtRouteResult("GetUsuario", new {id = user.Id},user);
+            return new CreatedAtRouteResult("GetUsuarioById", new {id = user.Id}, user);
         }
 
         //metodo put (editar) 
