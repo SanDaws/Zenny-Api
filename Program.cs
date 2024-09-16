6930b49e1ce596608dc07bff1ccd293ce8d9d9a4
Daws <castrogiraldosantiago@gmail.com>
2024-09-16 07:52:03 -0500

    hotfix: swagger avaliable

--- Program.cs
+++ Program.cs
@@ -51,11 +51,10 @@ public class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
 
         app.UseHttpsRedirection();
 
